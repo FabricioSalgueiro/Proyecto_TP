f14331f39bf547af3b102529acb59c2829864232
FabricioSalgueiro <salgueirofabricio6@gmail.com>
2025-11-12 18:44:28 -0300

    Update db.php

--- PROYECTO/libreria-online/config/db.php
+++ PROYECTO/libreria-online/config/db.php
@@ -1,23 +1,23 @@
 <?php
-// config/db.php
+
 
 $host = 'localhost';
-$db   = 'libreria_db'; // Nombre de la base de datos creada arriba
-$user = 'root';        // Usuario de MySQL (cambiar si es necesario)
-$pass = '';            // Contraseña de MySQL (cambiar si es necesario)
+$db   = 'libreria_db'; 
+$user = 'root';        
+$pass = '';            
 $charset = 'utf8mb4';
-$port = '3307';
+$port = '3306';
 
 $dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Traer resultados como array asociativo
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
 try {
      $pdo = new PDO($dsn, $user, $pass, $options);
-     // echo "Conexión exitosa."; // Descomentar para probar la conexión
+     
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
